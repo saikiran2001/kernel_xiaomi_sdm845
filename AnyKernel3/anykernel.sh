5a67bec9634c3f9fac342c94ba2f436c18ad0f9e
PainKiller3 <ninadpatil100@gmail.com>
2020-05-05 17:12:17 +0530

    kernel: Update AnyKernel3
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- AnyKernel3/anykernel.sh
+++ AnyKernel3/anykernel.sh
@@ -7,6 +7,7 @@ properties() { '
 kernel.string=
 do.devicecheck=1
 do.modules=0
+do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
 device.name1=beryllium
@@ -14,30 +15,18 @@ device.name2=PocoF1
 device.name3=PocophoneF1
 device.name4=
 device.name5=
-supported.versions=10,10.0
+supported.versions=10
+supported.patchlevels=
 '; } # end properties
 
 # shell variables
 block=/dev/block/bootdevice/by-name/boot;
-is_slot_device=auto;
+is_slot_device=0;
 ramdisk_compression=auto;
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
-
-# Set Android version for kernel
-ver="$(file_getprop /system/build.prop ro.build.version.release)"
-if [ ! -z "$ver" ]; then
-  patch_cmdline "androidboot.version" "androidboot.version=$ver"
-else
-  patch_cmdline "androidboot.version" ""
-fi
-
-## IMPORTANT NOTES
-ui_print " No MIUI support would be provided "
-ui_print " Flash Only On Rom With Q Blobs"
-ui_print " "
 
 ## AnyKernel install
 dump_boot;
@@ -49,4 +38,3 @@ fi;
 
 write_boot;
 ## end install
-
